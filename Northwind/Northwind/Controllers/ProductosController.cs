9d6081feb40733cd611c9c8cec2cfb2de7e13796
Daniel Salas <danielsd10@gmail.com>
2018-02-11 22:35:58 -0500

    Entity Framework: Productos

---
+++ Northwind/Northwind/Controllers/ProductosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Northwind.Controllers
+{
+    public class ProductosController : Controller
+    {
+        NorthwndDB DB = new NorthwndDB();
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(DB.Products);
+        }
+
+        // GET: Categorias/Nuevo
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+    }
+}
